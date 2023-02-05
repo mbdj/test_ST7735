b6034fa7ba5777f0e2f51e70b9a5b7364867fc4d
mehdi <mbdj@free.fr>
2023-02-05 17:29:17 +0100

    refactoring

--- src/st7735_buffering.ads
+++ src/st7735_buffering.ads
@@ -1,8 +1,10 @@
+with HAL.Time;
 with ST7735R;
 with Memory_Mapped_Bitmap;
 with HAL; use HAL;
-with STM32.GPIO;
 with SPI; use SPI;
+with HAL.SPI; use HAL.SPI;
+with HAL.GPIO; use HAL.GPIO;
 
 
 package ST7735_Buffering is
@@ -16,26 +18,32 @@ package ST7735_Buffering is
 --  Il faut refaire le dessin complet avant chaque Display
 --
 
-	type ST7735_Buffering is limited new ST7735R.ST7735R_Screen with private;
-
 	type Type_Orientation is (LANDSCAPE, PORTRAIT);
 
+
+	type ST7735_Buffering
+	  (Port                : not null Any_SPI_Port;
+	 CS                  : not null Any_GPIO_Point;
+	 RS                  : not null Any_GPIO_Point;
+	 RST                 : not null Any_GPIO_Point;
+	 Time                : not null HAL.Time.Any_Delays;
+	 Choix_SPI           :  SPI.Choix_SPI;
+	 SPI_SCK             :  not null Any_GPIO_Point;
+	 SPI_MISO            :  not null Any_GPIO_Point;
+	 SPI_MOSI            :  not null Any_GPIO_Point;
+	 Width               :  Natural;
+	 Height              :  Natural;
+	 Orientation         :  Type_Orientation;
+	 Color_Correction    :  Boolean)
+	is limited new ST7735R.ST7735R_Screen with private;
+
+
 	--  initialisation de l'écran et du buffer
-	procedure Initialize (ST7735       : in out ST7735_Buffering;
-							  Choix_SPI    : in SPI.Choix_SPI;
-							  SPI_SCK      : in STM32.GPIO.GPIO_Point;
-							  SPI_MISO     : in STM32.GPIO.GPIO_Point;
-							  SPI_MOSI     : in STM32.GPIO.GPIO_Point;
-							  PIN_RS       : in out STM32.GPIO.GPIO_Point;
-							  PIN_RST      : in out STM32.GPIO.GPIO_Point;
-							  PIN_CS       : in out STM32.GPIO.GPIO_Point;
-							  Width        : in Natural := 128;  --  les spécifications du ST7735 sont données en orientation portrait
-							  Height       : in Natural := 160;  --  les spécifications du ST7735 sont données en orientation portrait
-							  Orientation  : in Type_Orientation := LANDSCAPE);
+	procedure Initialize (ST7735 : in out ST7735_Buffering);
 
 	--  retourne la bitmap sur laquelle on peut dessiner
- --  pour écrire : utiliser le package Bitmapped_Drawing
- --  pour dessiner : utiliser le package HAL.Bitmap
+	--  pour écrire : utiliser le package Bitmapped_Drawing
+	--  pour dessiner : utiliser le package HAL.Bitmap
 	function BitMap (ST7735 : in out ST7735_Buffering) return Memory_Mapped_Bitmap.Any_Memory_Mapped_Bitmap_Buffer;
 
 	--  après avoir dessiner sur la bitmap il faut appeler Display pour afficher sur l'écran physique
@@ -44,20 +52,29 @@ package ST7735_Buffering is
 private
 
 	--  type ST7735_Buffering is record
-	type ST7735_Buffering is limited new ST7735R.ST7735R_Screen with record
-		Choix_SPI    :  SPI.Choix_SPI;
-		SPI_SCK      :  STM32.GPIO.GPIO_Point;
-		SPI_MISO     :  STM32.GPIO.GPIO_Point;
-		SPI_MOSI     :  STM32.GPIO.GPIO_Point;
-		PIN_RS       :  STM32.GPIO.GPIO_Point;
-		PIN_RST      :  STM32.GPIO.GPIO_Point;
-		PIN_CS       :  STM32.GPIO.GPIO_Point;
-		Width        :  Natural := 128;
-		Height       :  Natural := 160;
-
-
-		--  BitMap_Buffer pour le double buffering
-		--  voir https://github.com/AdaCore/Ada_Drivers_Library/blob/master/boards/OpenMV2/src/openmv-bitmap.adb
+	type ST7735_Buffering
+	  (Port                : not null Any_SPI_Port;
+	 CS                  : not null Any_GPIO_Point;
+	 RS                  : not null Any_GPIO_Point;
+	 RST                 : not null Any_GPIO_Point;
+	 Time                : not null HAL.Time.Any_Delays;
+	 Choix_SPI           :  SPI.Choix_SPI;
+	 SPI_SCK             :  not null Any_GPIO_Point;
+	 SPI_MISO            :  not null Any_GPIO_Point;
+	 SPI_MOSI            :  not null Any_GPIO_Point;
+	 Width               :  Natural;
+	 Height              :  Natural;
+	 Orientation         :  Type_Orientation;
+	 Color_Correction    :  Boolean)
+	is limited new ST7735R.ST7735R_Screen
+	  (Port         => Port,
+	 CS           => CS,
+	 RS           => RS,
+	 RST          => RST,
+	 Time         => Time)
+	with record
+	--  BitMap_Buffer pour le double buffering
+	--  voir https://github.com/AdaCore/Ada_Drivers_Library/blob/master/boards/OpenMV2/src/openmv-bitmap.adb
 		BitMap_Buffer            : Memory_Mapped_Bitmap.Any_Memory_Mapped_Bitmap_Buffer;
 		Pixel_Data_BitMap_Buffer : access HAL.UInt16_Array ;
 	end record;
